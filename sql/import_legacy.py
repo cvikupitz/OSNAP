24b176f40a1a887e568e56755104b47f0681124c
cvikupitz <cvikupit@uoregon.edu>
2017-01-24 09:09:27 -0800

    Uploaded import_legacy.py

--- sql/import_legacy.py
+++ sql/import_legacy.py
@@ -2,10 +2,111 @@
 import_legacy.py
 CIS 322 - Assignment 2
 Author - Cole Vikupitz
-------------------------------------------------------------------
+-----------------------------------------------------------------
 Imports the downloaded legacy data into the specified database.
+
+Usage:
+    >> python3 import_legacy.py [database] [port_number]
+
+Files:
+    aquisitions.csv
+    convoys.csv
+    DC_inventory.csv
+    HQ_inventory.csv
+    MB005_inventory.csv
+    NC_inventory.csv
+    product_list.csv
+    security_compartments.csv
+    security_levels.csv
+    SPNV_inventory.csv
+    transit.csv
+    vendors.csv
 """
 
+def acquisitions():
+    """
+    Imports legacy data from aquisitions.csv
+    """    
+    ### FIXME
+
+
+def convoys():
+    """
+    Imports legacy data from convoys.csv
+    """    
+    ### FIXME
+
+
+def DC_inventory():
+    """
+    Imports legacy data from DC_inventory.csv
+    """    
+    ### FIXME
+
+
+def HQ_inventory():
+    """
+    Imports legacy data from HQ_inventory.csv
+    """    
+    ### FIXME
+
+
+def MB005_inventory():
+    """
+    Imports legacy data from MB005_inventory.csv
+    """    
+    ### FIXME
+
+
+def NC_inventory():
+    """
+    Imports legacy data from NC_inventory.csv
+    """    
+    ### FIXME
+
+
+def product_list():
+    """
+    Imports legacy data from product_list.csv
+    """    
+    ### FIXME
+
+
+def security_compartments():
+    """
+    Imports legacy data from security_compartments.csv
+    """
+    
+    ### FIXME
+
+def security_levels():
+    """
+    Imports legacy data from security_levels.csv
+    """
+    ### FIXME
+    
+
+def SPNV_inventory():
+    """
+    Imports legacy data from SPNV_inventory.csv
+    """
+    ### FIXME
+
+
+def transit():
+    """
+    Imports legacy data from transit.csv
+    """    
+    ### FIXME
+
+
+def vendors():
+    """
+    Imports legacy data from vendors.csv
+    """    
+    ### FIXME
+
+
 if __name__ == "__main__":
-	print(" ***** FIXME ***** ")
-	print("Legacy data not imported yet.")
+
+    print("Hello")
