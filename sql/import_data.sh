ec13438716ae26d52b7dd3a916f93f45dd28cb74
cvikupitz <cvikupit@uoregon.edu>
2017-01-24 00:46:58 -0800

    Uploaded import_data.sh

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -6,4 +6,7 @@
 # Use curl to download the legacy data, unpack file
 curl -o osnap_legacy.tar.gz https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
 tar -xjf osnap_legacy.tar.gz
-cd osnap_legacy/
+
+# Create database, use create_tables.sql to generate tables
+psql --filename=create_tables.sql $1
+
---
+++ sql/import_legacy.py
@@ -0,0 +1,11 @@
+"""
+import_legacy.py
+CIS 322 - Assignment 2
+Author - Cole Vikupitz
+--------------------------------------------------------
+Imports the downloaded legacy data into the specified
+database.
+"""
+
+if __name__ == "__main__":
+	print("Hello")
