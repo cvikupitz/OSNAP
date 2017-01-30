a6e700cabe996eed3256116189e4b8a62f260022
cvikupitz <cvikupit@uoregon.edu>
2017-01-30 09:11:50 -0800

    Modified import_inventories.py, uploaded import_securities.py

--- sql/import_inventories.py
+++ sql/import_inventories.py
@@ -18,30 +18,41 @@ Files:
 
 import sys, csv, psycopg2
 
-def import_inventory(name, base):
+def import_inventory(name, base, port_num):
+
     """
     Imports the legacy data from all the inventory files to the database.
     """
+
+    # Opens the csv file
     file = open(name)
     reader = csv.reader(file)
     data = list(reader)
-	conn = psycopg2.connect(dbname = base, host = "localhost")
+
+    # Opens the database
+    conn = psycopg2.connect(dbname = base, host = "localhost", port = port_num)
     curr = conn.cursor()
-	for dt in data[1:]:
+
+    # Inserts the data in the appropriate places
+    for dt in data[1:]:
         curr.execute("INSERT INTO assets (product_pk, asset_tag, description) VALUES (NULL, %s, %s)", dt[0], dt[1])
+
+    # Close the database
     conn.close()
     curr.close()
     
 
 if __name__ == "__main__":
 
+    # Incorrect usage
     if len(sys.argv) != 3:
         print("Usage:")
         print("\t>> python3 import_legacy.py [database_name] [port_number]")
         sys.exit()
 
-    import_inventory("DC_inventory.csv", sys.argv[1])
-    import_inventory("HQ_inventory.csv", sys.argv[1])
-    import_inventory("MB005_inventory.csv", sys.argv[1])
-    import_inventory("NC_inventory.csv", sys.argv[1])
-    import_inventory("SPNV_inventory.csv", sys.argv[1])
+    # Execute the imports
+    import_inventory("DC_inventory.csv", sys.argv[1], sys.argv[2])
+    import_inventory("HQ_inventory.csv", sys.argv[1], sys.argv[2])
+    import_inventory("MB005_inventory.csv", sys.argv[1], sys.argv[2])
+    import_inventory("NC_inventory.csv", sys.argv[1], sys.argv[2])
+    import_inventory("SPNV_inventory.csv", sys.argv[1], sys.argv[2])
