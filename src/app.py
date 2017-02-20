4c40bbfda1a53113df81b70125ec00c4e927a981
cvikupitz <cvikupit@uoregon.edu>
2017-02-19 20:25:35 -0800

    Moved app.py

---
+++ src/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("src/login.html")
+
+
+##@app.route('/login')
+##def login():
+##    return "Login...."
+##
+##
+##@app.route('/filter')
+##def filter():
+##    return "Filter...."
+##
+##
+##@app.route('/inventory')
+##def inventory():
+##    return "Inventory...."
+##
+##
+##@app.route('/transit')
+##def transit():
+##    return "Transit...."
+##
+##
+##@app.route('/logout')
+##def logout():
+##    return "Logout...."
+
+	
+if __name__ == "__main__":
+    app.run()
