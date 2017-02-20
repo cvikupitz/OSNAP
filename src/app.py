a3792867ee2a40416d261e028ed68068a5fd9649
cvikupitz <cvikupit@uoregon.edu>
2017-02-19 20:29:01 -0800

    Deleted clients/

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("src/login.html")
+    return render_template("login.html")
 
 
 ##@app.route('/login')
--- src/clients/README.txt
+++
@@ -1,9 +0,0 @@
-..
-|
-|-- activate_user.py
-|-- add_asset.py
-|-- add_product.py
-|-- list_products.py
-|-- lost_key.py
-|-- suspend_user.py
-|
