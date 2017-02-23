ea8c89291a42a24b9c63d92a4d5056bb58791103
cvikupitz <cvikupit@uoregon.edu>
2017-02-23 14:27:24 -0800

    Modified app.py

--- src/app.py
+++ src/app.py
@@ -4,33 +4,57 @@ Author: Cole Vikupitz
 CIS 322 Assignment 6
 """
 
-
+# Imports
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+"""
+Login page for the users. User can login with a username
+and password, and will link to dashboard if the login
+was successful, or notify user if unsuccessful.
+"""
 
+##@app.route('/login', methods = ['GET','POST'])
+##def login():
+##    return """"""
+##
+##
+
+
+"""
+Send the user to the create user screen. Here, users can
+create a new account by giving a username and password
+16 characters or less. Checks to see if the new info
+exists and alerts users if so.
+"""
 @app.route('/')
-@app.route('/login')
-def login():
-    return ""
-
-
 @app.route('/create_user', methods = ['GET','POST'])
 def create_user():
     if (request.method == 'GET'):
-        return render_template("create_user.html")
+        return render_template('create_user.html')
     else:
-        return render_template("login.html")
+        ####### FIXME
+        return render_template('login.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return ""
 
-@app.route('/logout')
+"""
+Send the user to the dashboard screen upon login. Displays
+the username on the screen. User should be able to logout
+from here.
+"""
+@app.route('/dashboard/<user>', methods = ['GET'])
+def dashboard(user):
+    return render_template('dashboard.html', name = user)
+
+"""
+User has logged out of the system, go to the logout page, link
+user back to the login page upon request.
+"""
+@app.route('/logout', methods = ['GET'])
 def logout():
-    return ""
+    return render_template('logout.html')
 
 	
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
