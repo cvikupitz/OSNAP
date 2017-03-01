46a560cdc25e515cd1d97aab63d0aff37cc06d74
cvikupitz <cvikupit@uoregon.edu>
2017-02-28 19:29:33 -0800

    ,

--- src/util.py
+++ src/util.py
@@ -11,6 +11,13 @@ def date_valid(date):
     """
     import re
     regex = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}')
-    return regex.match(date)
+    if (regex.match(date)):
+        return True
+    return False
 
-
+if __name__ == "__main__":
+    print(date_valid('1999-12-12'))
+    print(date_valid('2222-33-23'))
+    print(date_valid('33-12-12'))
+    print(date_valid('199-12-12'))
+    print(date_valid('1999-12w-12'))
