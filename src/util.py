270cc18e442d2730e1c237c6c20dee80286299fb
cvikupitz <cvikupitz@gmail.com>
2017-03-01 03:26:03 +0000

    Modified util.py

--- src/util.py
+++ src/util.py
@@ -4,4 +4,13 @@ Author: Cole Vikupitz
 CIS 322
 """
 
+def date_valid(date):
+
+    """
+
+    """
+    import re
+    regex = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}')
+    return regex.match(date)
+
 
