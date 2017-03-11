f7193f9366a2626b33d3efcd9a4e6a635a29456e
cvikupitz <cvikupitz@gmail.com>
2017-03-11 00:26:03 +0000

    Fixed directory.py

--- export/directory.py
+++ export/directory.py
@@ -2,6 +2,9 @@
 directory.py
 Author: Cole Vikupitz
 
+User passes in a directory to be created. If the directory does not exist, it
+gets created, or its contents are cleared if it does exist.
+
 Usage:
     >> python3 directory.py [dir]
 """
@@ -19,11 +22,15 @@ if __name__ == "__main__":
         print("\t>> python3 directory.py [dir]")
 
     # Checks to see if the directory exists, if not, create it.
-    directory = argv[1]
-    if (os.path.exists(directory)):
-        print("That path exists.")
+    directory = sys.argv[1]
+    if not (os.path.exists(directory)):
+        os.makedirs(directory)
 
     # If the directory exists, remove all contents inside.
     else:
-        print("That path does not exist.")
+        files = os.listdir(directory)
+        for f in files:
+            temp = os.path.join(directory, f)
+            if os.path.isfile(temp):
+                os.remove(temp)
     
