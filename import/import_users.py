6c20cfe259e0b784b8673de234742701b6670894
cvikupitz <cvikupit@uoregon.edu>
2017-03-12 11:47:44 -0700

    Modified import users

--- import/import_users.py
+++ import/import_users.py
@@ -2,7 +2,9 @@
 import_users.py
 Author: Cole Vikupitz
 
-FIXME
+Imports information from the file 'users.csv' into the users table in the specified
+database from the indicated directory. The data that is imported is committed to the
+new database.
 
 Usage:
     >> python3 import_users.py [dbname] [input_dir]
@@ -10,14 +12,31 @@ Usage:
 
 # Imports
 import sys
-from csv import *
+import os
+import csv
+import psycopg2
 
 
 """
 FIXME
 """
-def user_import(dbname, input):
-    ### FIXME
+def user_import(name, directory):
+    with psycopg2.connect(dbname = name, host = '127.0.0.1', port = 5432) as conn:
+        cur = conn.cursor()
+        inputfile = open(os.path.join(directory, 'users.csv'))
+        reader = csv.reader(inputfile)
+        users = list(reader)
+
+        for user in users[1:]:
+            if user[2] == 'Logistics Officer':
+                role = 1
+            else:
+                role = 2
+            cur.execute("INSERT INTO users (username, password, role, active) VALUES (%s, %s, %s, %s)",
+                        (user[0], user[1], role, user[3]))
+            conn.commit()
+        inputfile.close()
+        
 
 
 if __name__ == "__main__":
@@ -29,4 +48,9 @@ if __name__ == "__main__":
         sys.exit()
 
     # Execute the import
-    print("Hello")
+    dbname = sys.argv[1]
+    directory = sys.argv[2]
+    user_import(dbname, directory)
+
+
+    
